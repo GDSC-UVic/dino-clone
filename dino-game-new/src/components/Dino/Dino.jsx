2613bb660d6492c170eb2d7d2896d58cd0d12be6
Maika Coleen Rabenitas <maikacoleen1205@gmail.com>
2023-11-14 12:34:52 -0800

    section 1 answers
    code answer for section 1

--- dino-game-new/src/components/Dino/Dino.jsx
+++ dino-game-new/src/components/Dino/Dino.jsx
@@ -1,34 +1,34 @@
-// DO: import useState hook and useRef hook
-// Code goes here
+// import useState hook and useRef hook
+import React, { useRef, useState } from "react";
 // DO: import css file
-// Code goes here
+import "./Dino.css";
 
 function Dino() {
   // DO: ref to get 'dino' html element in js
   // Code goes here
- 
+  const dinoRef = useRef();
   // DO: ref to get 'cactus' html element in js
   // Code goes here
-
+  const cactusRef = useRef();
   // DO: create state for score
   // DO: set initial score to 0
   // Hint! use useState hook
   // Code goes here
-
+  const [score, setScore] = useState(0);
 
   return (
     <div className="game">
       {/* DO: using the h2 tag create a score board */}
       {/* Hint! Make use of the usestate you created */}
-      {/* Code goes here */}
+      <h2>Score: {score}</h2>
 
       {/* DO: create the dino */}
       {/* Hint! Make use of the useRef */}
-      {/* Code goes here */}
+      <div id="dino" ref={dinoRef}></div>
 
       {/* DO: create the cactus */}
       {/* Hint! Make use of the useRef */}
-      {/* Code goes here */}
+      <div id="cactus" ref={cactusRef}></div>
     </div>
   );
 }
