0733dfaa7d5bb3b80be998a14911f66ffa85d14e
Maika Coleen Rabenitas <maikacoleen1205@gmail.com>
2023-11-14 13:23:14 -0800

    Answer code for section two

--- dino-game-new/src/components/Dino/Dino.jsx
+++ dino-game-new/src/components/Dino/Dino.jsx
@@ -1,5 +1,4 @@
-// DO: Import useEffect hook
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Dino.css";
 
 function Dino() {
@@ -9,36 +8,26 @@ function Dino() {
   const [score, setScore] = useState(0);
 
   useEffect(() => {
-    
     const isAlive = setInterval(function () {
-      // DO: Inside the setInterval function, get the current dino and cactus position.
-      // Hint! get current dino Y position
-      // Hint! parseInt() function converts a string to an integer.
-      // Hint! getComputedStyle() function returns the values of all the CSS properties of an element.
-      // Code goes here ...
       
+      const dinoTop = parseInt(
+        getComputedStyle(dinoRef.current).getPropertyValue("top")
+      );
 
-      // DO: Do the same exact thing for the cactus position.
-      // Hint! get current cactus X position
-      // Code goes here ...
-      
+      let cactusLeft = parseInt(
+        getComputedStyle(cactusRef.current).getPropertyValue("left")
+      );
 
-      // DO: Check for collision.
-      // DO: Check if the cactus is less than 40px from the left and greater than 0px.
-      // DO: Check if the dino is less than 140px from the top.
-      // Hint! use if statement
-      // DO: If the above conditions are true, then alert "Game Over! Your Score : " + score by using alert() function.
-      // DO: If the above conditions are false, then increment the score by 1.
-      // Hint! use setScore() function
-      // Code goes here ...
+      if (cactusLeft < 40 && cactusLeft > 0 && dinoTop >= 140) {
+        alert( "Game Over! Your Score : " + score);
+        setScore(0);
 
-
+      } else {
+        setScore(score + 1);
+      }
     }, 10);
-    
-    // DO: Change the code below to return the clearInterval function.
-    // This is to clear the interval when the component is unmounted.
-    // Code goes here ...
-    return () => {};
+
+    return () => clearInterval(isAlive);
   });
 
   return (
