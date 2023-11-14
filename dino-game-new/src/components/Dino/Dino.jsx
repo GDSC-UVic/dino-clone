fbf1ea0ce02182313b92a33abde88fd33035e26e
Maika Coleen Rabenitas <maikacoleen1205@gmail.com>
2023-11-14 12:33:01 -0800

    Section 1 skeleton code
    created the skeleton code focused on importing creating variables and images. Commented instruction for users to follow.

--- dino-game-new/src/components/Dino/Dino.jsx
+++ dino-game-new/src/components/Dino/Dino.jsx
@@ -1,62 +1,34 @@
-import React, { useEffect, useRef, useState } from "react";
-import "./Dino.css";
+// DO: import useState hook and useRef hook
+// Code goes here
+// DO: import css file
+// Code goes here
 
 function Dino() {
-  //ref to get 'dino' html element in js
-  const dinoRef = useRef();
-  //ref to get 'cactus' html element in js
-  const cactusRef = useRef();
-  const [score, setScore] = useState(0);
+  // DO: ref to get 'dino' html element in js
+  // Code goes here
+ 
+  // DO: ref to get 'cactus' html element in js
+  // Code goes here
 
-  //method to add 'jump' class every '300ms' as the class jump css has jumping animation of 0.3s(300ms).
-  //so on each key press we need to add animation and remove animation
-  const jump = () => {
-    if (!!dinoRef.current && dinoRef.current.classList !== "jump") {
-      dinoRef.current.classList.add("jump");
-      setTimeout(function () {
-        dinoRef.current.classList.remove("jump");
-      }, 300);
-    }
-  };
+  // DO: create state for score
+  // DO: set initial score to 0
+  // Hint! use useState hook
+  // Code goes here
 
-  //useEffect to track whether dino position and cactus position is intersecting
-  //if yes, then game over.
-  useEffect(() => {
-    const isAlive = setInterval(function () {
-      // get current dino Y position
-      const dinoTop = parseInt(
-        getComputedStyle(dinoRef.current).getPropertyValue("top")
-      );
-
-      // get current cactus X position
-      let cactusLeft = parseInt(
-        getComputedStyle(cactusRef.current).getPropertyValue("left")
-      );
-
-      // detect collision
-      if (cactusLeft < 40 && cactusLeft > 0 && dinoTop >= 140) {
-        // collision
-        alert( "Game Over! Your Score : " + score);
-        setScore(0);
-      } else {
-        setScore(score + 1);
-      }
-    }, 10);
-
-    return () => clearInterval(isAlive);
-  });
-
-  //hook to call jump method on any keypress
-  useEffect(() => {
-    document.addEventListener("keydown", jump);
-    return () => document.removeEventListener("keydown", jump);
-  }, []);
 
   return (
     <div className="game">
-      Score : {score}
-      <div id="dino" ref={dinoRef}></div>
-      <div id="cactus" ref={cactusRef}></div>
+      {/* DO: using the h2 tag create a score board */}
+      {/* Hint! Make use of the usestate you created */}
+      {/* Code goes here */}
+
+      {/* DO: create the dino */}
+      {/* Hint! Make use of the useRef */}
+      {/* Code goes here */}
+
+      {/* DO: create the cactus */}
+      {/* Hint! Make use of the useRef */}
+      {/* Code goes here */}
     </div>
   );
 }
